e0007270193c30957e90741310e37171234a4c25
Thomas Banister <tgbanister@gmail.com>
2019-08-13 16:31:51 +0100

    rearrange

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ var rail = ukTrains(process.env.TRAIN_KEY)
 
 const getDepartures = (a, b) => new Promise(resolve => rail.getDepartures(a, b, resolve))
 
-const handler = async () => {
+const handler = async data => {
+  console.log(data)
   try {
     const trains = await getDepartures('LDS', { toStation: 'HDY' })
 
-    return { body: JSON.stringify(trains) }
+    return { headers: { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': '*', 'Access-Control-Allow-Methods': 'GET, OPTIONS' }, body: JSON.stringify(trains) }
   } catch (e) {
     console.log('in error', e)
   }
