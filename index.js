44d1c01d4b3c42ee15e50c696a84d262114517c5
Thomas Banister <tgbanister@gmail.com>
2019-08-13 16:56:27 +0100

    wip

--- index.js
+++ index.js
@@ -5,10 +5,9 @@ var rail = ukTrains(process.env.TRAIN_KEY)
 
 const getDepartures = (a, b) => new Promise(resolve => rail.getDepartures(a, b, resolve))
 
-const handler = async data => {
-  console.log(data)
+const handler = async ({ queryStringParameters: { from = 'LDS', to = 'HDY' } }) => {
   try {
-    const trains = await getDepartures('LDS', { toStation: 'HDY' })
+    const trains = await getDepartures(from, { toStation: to })
 
     return { headers: { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': '*', 'Access-Control-Allow-Methods': 'GET, OPTIONS' }, body: JSON.stringify(trains) }
   } catch (e) {
