870351c3e8b801e0aa8c244a03f2837142ddcc15
Thomas Banister <tgbanister@gmail.com>
2019-08-13 17:12:24 +0100

    gg

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var rail = ukTrains(process.env.TRAIN_KEY)
 
 const getDepartures = (a, b) => new Promise(resolve => rail.getDepartures(a, b, resolve))
 
-const handler = async ({ queryStringParameters: { from = 'LDS', to = 'HDY' } }) => {
+const handler = async data => {
+  const { from = 'LDS', to = 'HDY' } = data.queryStringParameters || {}
   try {
     const trains = await getDepartures(from, { toStation: to })
 
